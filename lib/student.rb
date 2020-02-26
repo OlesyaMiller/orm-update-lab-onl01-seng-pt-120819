bdea8f57d728996c843bb449dcd2dd37d947cd27
github username <github email address>
2020-02-26 16:36:12 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,61 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade
+  attr_reader :id 
 
+  def initialize(id = nil, name, grade)
+    @name = name
+    @grade = grade
+    @id = id 
+  end
+
+  def self.create_table
+    sql = "CREATE TABLE students
+          (id INTEGER PRIMARY KEY,
+          name TEXT,
+          grade INTEGER)"
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE students"
+
+    DB[:conn].execute(sql)
+  end
+
+  def save 
+    if self.id 
+      self.update
+    else
+      sql = "INSERT INTO students (name, grade)
+            VALUES (?, ?)"
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save    
+    student 
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    returned_row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(returned_row)
+  end 
+
+  def self.new_from_db(returned_row)
+    new_student = self.new(returned_row[0], returned_row[1], returned_row[2])
+    new_student
+  end
 end
